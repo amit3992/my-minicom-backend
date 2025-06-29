1b3bdc25871435dddd78a73577d9f31e154e2105
Amit Kulkarni <amit3992@gatech.edu>
2025-06-28 22:29:16 -0700

    app upgrades to java 17

--- src/main/java/com/intercom/spring/rest/controllers/BarController.java
+++ src/main/java/com/intercom/spring/rest/controllers/BarController.java
@@ -1,17 +1,20 @@
-package com.intercom.spring.controllers;
+package com.intercom.spring.rest.controllers;
 
 import java.util.Map;
 import java.util.HashMap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 public class BarController {
 
     @PostMapping("/bar")
     public Map verify() {
+        log.info("bar");
         HashMap<String, Boolean> map = new HashMap<>();
         map.put("success", true);
         return map;
